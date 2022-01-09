b4918a942c19d8c8fbcce07d160bdb7cec01a16b
Muthu Kannan <m@manki.in>
2022-01-09 15:51:13 +0530

    Update the extension to Manifest V3. Also remove support for legacy Blogger URL.

--- background.js
+++ background.js
@@ -1,23 +1,27 @@
-var BLOGGER_NEW_UI_EDITOR_URL = /^https?:\/\/(?:www|draft)\.blogger\.com\/blog\/post\/edit\//;
-var BLOGGER_OLD_UI_EDITOR_URL = /^https?:\/\/(?:www|draft)\.blogger\.com\/blogger\.g\?.*#editor\b/;
-var GMAIL_URL = /^https:\/\/mail\.google\.com\//;
+const BLOGGER_EDITOR_URL = '^https?\\://(?:www|draft)\\.blogger\\.com/blog/post/edit/';
+const GMAIL_URL = '^https\\://mail\\.google\\.com/';
 
-function onTabEvent(tabId) {
-  chrome.tabs.get(tabId, function(tab) {
-    if (BLOGGER_NEW_UI_EDITOR_URL.test(tab.url) ||
-        BLOGGER_OLD_UI_EDITOR_URL.test(tab.url) ||
-        GMAIL_URL.test(tab.url)) {
-      chrome.pageAction.show(tab.id);
-    } else {
-      chrome.pageAction.hide(tab.id);
-    }
+const rule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {
+        originAndPathMatches: `(?:${BLOGGER_EDITOR_URL})|(?:${GMAIL_URL})`,
+      },
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()],
+};
+
+function onActionClick(tab) {
+  chrome.scripting.executeScript({
+    'target': {tabId: tab.id},
+    'files': ['changequotes.js'],
   });
 }
 
-function onPageActionClick(tab) {
-  chrome.tabs.executeScript(tab.id, {'file': 'changequotes.js'});
-}
-
-chrome.tabs.onUpdated.addListener(onTabEvent);
-chrome.tabs.onActiveChanged.addListener(onTabEvent);
-chrome.pageAction.onClicked.addListener(onPageActionClick);
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
+  });
+});
+chrome.action.onClicked.addListener(onActionClick);
